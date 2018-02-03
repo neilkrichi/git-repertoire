1c479a84a0aa21181434cf62459f62700bfa508e
neilkrichi <neilkrichi@gmail.com>
2018-02-02 16:44:45 -0800

    added user component and header tings

--- src/Header.js
+++ src/Header.js
@@ -6,7 +6,9 @@ export default class Header extends Component {
   render(){
     return(
       <div className="App-header">
-        <h1 className="App-title">Welcome to React</h1>
+        <h1 className="App-title">Github User Search</h1>
+        <h3>Find Github users</h3>
+        <p>Powererd by the <a>GitHub API</a></p>
         <SearchBar />
       </div>
     )
