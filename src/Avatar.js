0201932a50fe890222ea70fbe4c55da9d5aff001
neilkrichi <neilkrichi@gmail.com>
2018-02-05 23:58:17 -0800

    added avatar component

---
+++ src/Avatar.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './App.css';
+
+export default class Avatar extends Component {
+  render(){
+    return(
+      <div className="avatar">
+        <img src='#' />
+      </div>
+    )
+  }
+}
